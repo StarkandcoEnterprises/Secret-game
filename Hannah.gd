663c695e5887898211fd2ff17aa59d306a5ce115
Starkandco <Starkandco@hotmail.com>
2023-11-16 10:29:17 +0000

    Hoe, inventory selection, equip
    Player can now use a cursor to navigate their inventory (wasd), and select items that have context menus using interact ("e").
    A hoe placeholder was added with an equip option in its context menu that reparents it to the player

--- Hannah.gd
+++ Hannah.gd
@@ -7,33 +7,43 @@ var direction = "right"
 @onready var inventory = get_tree().get_nodes_in_group("Inventory")[0]
 
 func _input(event):
-	if event.is_action_pressed("interact") and $RayCast2D.is_colliding():
-		pass
+	if !inventory.visible and event.is_action_pressed("interact") and $RayCast2D.is_colliding():
+		pass #some interact call on item here
+	elif inventory.visible and event.is_action_pressed("interact"):
+		inventory.get_context_menu_for_selected()
 	elif event.is_action_pressed("inventory"):
 		if inventory.visible:
 			inventory.hide() 
 		else:
 			inventory.show()
+	elif inventory.visible and event.is_action_pressed("right"):
+		inventory.move_cursor("right")
+	elif inventory.visible and event.is_action_pressed("left"):
+		inventory.move_cursor("left")
+	elif inventory.visible and event.is_action_pressed("up"):
+		inventory.move_cursor("up")
+	elif inventory.visible and event.is_action_pressed("down"):
+		inventory.move_cursor("down")
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("right") and !inventory.visible:
 		velocity.x += speed
 		face_right()
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") and !inventory.visible:
 		velocity.x -= speed
 		face_left()
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("down") and !inventory.visible:
 		velocity.y += speed
 		face_down()
-	if Input.is_action_pressed("up"):
+	if Input.is_action_pressed("up") and !inventory.visible:
 		velocity.y -= speed
 		face_up()
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		if collision.get_collider().is_in_group("Item"):
-			collision.get_collider().get_child(1).disabled = true
 			inventory.add_item(collision.get_collider())
+			collision.get_collider().get_child(1).queue_free()
 
 func face_right():
 	rotation_degrees = 0
@@ -54,3 +64,10 @@ func face_up():
 	rotation_degrees = 270
 	$Sprite2D.flip_v = false
 	direction = "up"
+
+func _on_equip_pressed():
+	var item = inventory.get_child(inventory.grid_pos).get_child(inventory.cursor_pos).get_child(0)
+	inventory.get_context_menu_for_selected()
+	inventory.remove_item(item, self)
+	inventory.get_child(1).show()
+	
--- PlayerInventoryUI.gd
+++ PlayerInventoryUI.gd
@@ -1,8 +1,43 @@
 extends Control
 
 var item_count = 0
+var grid_pos = 0
+var cursor_pos = 0
 
 func add_item(item):
-	item.reparent(get_child(0).get_child(item_count))
+	item.reparent(get_child(grid_pos).get_child(item_count))
 	item.position = Vector2(26,39) 
 	item_count += 1
+
+func delete_item(pos):
+	get_child(grid_pos).get_child(pos).get_child(0).queue_free()
+	item_count -= 1
+
+func remove_item(item, new_parent):
+	item.reparent(new_parent)
+	item_count -= 1
+
+func move_cursor(direction):
+	if direction == "right":
+		$ReferenceRect.position.x += 57.9
+		cursor_pos += 1
+	if direction == "left":
+		$ReferenceRect.position.x -= 57.9
+		cursor_pos -= 1
+	if direction == "up":
+		$ReferenceRect.position.y -= 81.5
+		cursor_pos -= 20
+	if direction == "down":
+		$ReferenceRect.position.y += 81.5
+		cursor_pos += 20
+
+func get_context_menu_for_selected():
+	if is_instance_valid(get_child(grid_pos).get_child(cursor_pos).get_child(0)):
+		if is_instance_valid(get_child(grid_pos).get_child(cursor_pos).get_child(0).get_child(1)):
+			$ReferenceRect.hide()
+			var selected = get_child(grid_pos).get_child(cursor_pos).get_child(0).get_child(1)
+			if selected.visible:
+				selected.hide()
+			else:
+				selected.show()
+
