e2a2c88c6c1297284d5810f6deb3e961faf17542
Starkandco <Starkandco@hotmail.com>
2023-11-16 18:17:29 +0000

    Inventory improvements
    Better approach to checking for children than valid instance and added sorting to inventory

--- PlayerInventoryUI.gd
+++ PlayerInventoryUI.gd
@@ -12,10 +12,26 @@ func add_item(item):
 func delete_item(pos):
 	get_child(grid_pos).get_child(pos).get_child(0).queue_free()
 	item_count -= 1
+	sort()
 
 func remove_item(item, new_parent):
 	item.reparent(new_parent)
 	item_count -= 1
+	sort()
+
+func sort():
+	var earliest_empty_spot
+	#Loop through all spots in inv
+	for i in range(0,160,1):
+		#when we get to the first empty spot, mark this as our starting empty spot
+		if get_child(grid_pos).get_child(i).get_child_count() == 0:
+			if earliest_empty_spot == null:
+				earliest_empty_spot = i
+		#when we find a child and there's an empty space, move it, and mark the next space as empty
+		elif earliest_empty_spot != null:
+			get_child(grid_pos).get_child(i).get_child(0).reparent(get_child(grid_pos).get_child(earliest_empty_spot))
+			get_child(grid_pos).get_child(earliest_empty_spot).get_child(0).position = Vector2(26,39) 
+			earliest_empty_spot += 1
 
 func move_cursor(direction):
 	if direction == "right":
@@ -33,8 +49,9 @@ func move_cursor(direction):
 
 func get_context_menu_for_selected():
 	#some checking to see that it's a valid instance so no null pointers
-	if is_instance_valid(get_child(grid_pos).get_child(cursor_pos).get_child(0)):
-		if is_instance_valid(get_child(grid_pos).get_child(cursor_pos).get_child(0).get_child(1)):
+	if get_child(grid_pos).get_child(cursor_pos).get_child_count() > 0:
+		#Not only does it need to have a sprite but also a context menu...
+		if get_child(grid_pos).get_child(cursor_pos).get_child(0).get_child_count() > 1:
 			#Don't want our cursor getting in the way of the mouse (Bad scene layout I presume)
 			$ReferenceRect.hide()
 			var selected = get_child(grid_pos).get_child(cursor_pos).get_child(0).get_child(1)
